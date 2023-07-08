a55d4a53fc9dd7f38f085ee113cea8d2188077de
mari nonikashvili <marinonikashvilii@gmail.com>
2023-07-08 12:03:29 +0400

    simple calculator

--- Program.cs
+++ Program.cs
@@ -3,6 +3,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Please, enter your first number");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(num1);
+
+        Console.WriteLine("Please, enter your second number");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(num2);
+
+        Console.WriteLine("Please, enter your math operator");
+        string mathOperator = Console.ReadLine();
+
+        string result;
+
+        switch (mathOperator)
+        {
+            case "+":
+                result = Convert.ToString(num1) + mathOperator + Convert.ToString(num2) + "=" + Convert.ToString(num1 + num2);
+                break;
+
+            case "-":
+                result = Convert.ToString(num1) + mathOperator + Convert.ToString(num2) + "=" + Convert.ToString(num1 - num2);
+                break;
+
+            case "*":
+                result = Convert.ToString(num1) + mathOperator + Convert.ToString(num2) + "=" + Convert.ToString(num1 * num2);
+                break;
+
+            case "/":
+                result = Convert.ToString(num1) + mathOperator + Convert.ToString(num2) + "=" + Convert.ToString(num1 / num2);
+                break;
+
+            case "%":
+                result = Convert.ToString(num1) + mathOperator + Convert.ToString(num2) + "=" + Convert.ToString(num1 % num2);
+                break;
+
+            default:
+                result = "Your operatin is failed";
+                break;
+        }
+
+        Console.WriteLine(result);
     }
 }
