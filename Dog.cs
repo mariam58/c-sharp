92aa8087d59002d3edd17ed17f0c90c517ad5865
mari nonikashvili <marinonikashvilii@gmail.com>
2023-07-13 16:56:18 +0400

    lesson2 homework

---
+++ Dog.cs
@@ -0,0 +1,51 @@
+// namespace c_sharp
+// {
+//     public class Dog
+//     {
+//         public static void Main(string[] args)
+//         {
+//             List<DogModel> dogs = new List<DogModel>();
+
+//             DogModel dog1 = new DogModel("Bull Terrier", 1.34d, 35d, "long");
+//             DogModel dog2 = new DogModel("Staffy", 2.34d, 35.4d, "short");
+//             var dog3 = new DogModel("Cane Corso", 2.34d, 35.4d, "short");
+//             var dog4 = new DogModel("beagles", 2.34d, 35.4d, "short");
+
+//             dogs.Add(dog1);
+//             dogs.Add(dog2);
+//             dogs.Add(dog3);
+//             dogs.Add(dog4);
+
+//             foreach (var item in dogs)
+//             {
+//                 if (item.Breed == "Bull Terrier")
+//                 {
+//                     Console.WriteLine("გრძელი სახე, მოკლე ბეწვი, ცოტა შეუხედავი");
+//                 }
+//                 if (item.Breed == "Staffy")
+//                 {
+//                     Console.WriteLine("მოკლე სახე, მოკლე ბეწვი, ლამაზი");
+//                 }
+//                 Console.WriteLine(item.Breed);
+//                 Console.WriteLine(item.Height);
+//                 Console.WriteLine(item.Weight);
+//                 Console.WriteLine(item.Fur);
+//             }
+//         }
+//     }
+//     public class DogModel
+//     {
+//         public string Breed { get; set; }
+//         public double Height { get; set; }
+//         public double Weight { get; set; }
+//         public string Fur { get; set; }
+
+//         public DogModel(string breed, double height, double weight, string fur)
+//         {
+//             Breed = breed;
+//             Height = height;
+//             Weight = weight;
+//             Fur = fur;
+//         }
+//     }
+// }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,57 @@
-﻿namespace c_sharp;
-class Program
+﻿namespace c_sharp
 {
-    static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello, World!");
+        public static void Main(string[] args)
+        {
+            List<BookModel> books = new List<BookModel>();
+            bool IsFinished = false;
+            while (!IsFinished)
+            {
+                Console.WriteLine("provide book title");
+                string bookTitle = Console.ReadLine();
+
+                Console.WriteLine("provide book author");
+                string bookAuthor = Console.ReadLine();
+
+                Console.WriteLine("provide book genre");
+                string bookGenre = Console.ReadLine();
+
+                BookModel book = new BookModel(bookTitle, bookAuthor, bookGenre);
+                books.Add(book);
+
+                Console.WriteLine("do you whant to continue?");
+
+                string answer = Console.ReadLine();
+                if (answer == "No")
+                {
+                    IsFinished = true;
+                    foreach (var item in books)
+                    {
+                        Console.WriteLine(item.Title);
+                        Console.WriteLine(item.Author);
+                        Console.WriteLine(item.Genre);
+                    }
+                }
+
+            }
+
+
+        }
+
+    }
+
+    public class BookModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+
+        public BookModel(string title, string author, string genre)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+        }
     }
 }
