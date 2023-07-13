e85cae5036b7c3808c130993bc13bd8f510be351
mari nonikashvili <marinonikashvilii@gmail.com>
2023-07-13 19:40:26 +0400

    lesson3 homework: Prime and Composite numbers

--- Program.cs
+++ Program.cs
@@ -3,6 +3,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        try
+        {
+            Console.WriteLine("შეიყვანეთ რიცხვი:");
+            int number = int.Parse(Console.ReadLine());
+            if (number > 1)
+            {
+                int i=2;
+                Boolean IsPrime = true;
+                while (i <= number / 2)
+                {
+                    if (number % i == 0)
+                    {
+                        IsPrime = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (IsPrime == true)
+                {
+                    Console.WriteLine("თქვენი შერჩეული რიცხვი მარტივია");
+                }
+                else
+                {
+                    Console.WriteLine("თქვენი შერჩეული რიცხვი შედგენილია");
+                }
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+        catch
+        {
+            Console.WriteLine("შეიყვანეთ ვალიდური ციფრი");
+        }
     }
 }
